5dbb5da914259cf5d394343975c931ab424fb949
Sebastián Passaro <sebastian.passaro@owasp.org>
2024-08-17 19:05:44 -0300

    Add serializeElement override form DOM output

--- src/main/java/org/owasp/validator/html/scan/ASHTMLSerializer.java
+++ src/main/java/org/owasp/validator/html/scan/ASHTMLSerializer.java
@@ -2,6 +2,8 @@ package org.owasp.validator.html.scan;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Locale;
+
 import org.apache.xml.serialize.ElementState;
 import org.apache.xml.serialize.HTMLdtd;
 import org.apache.xml.serialize.OutputFormat;
@@ -9,6 +11,10 @@ import org.owasp.validator.html.InternalPolicy;
 import org.owasp.validator.html.TagMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 
 @SuppressWarnings("deprecation")
 public class ASHTMLSerializer extends org.apache.xml.serialize.HTMLSerializer {
@@ -30,6 +36,139 @@ public class ASHTMLSerializer extends org.apache.xml.serialize.HTMLSerializer {
     if (encodeAllPossibleEntities || Constants.big5CharsToEncode.indexOf(charToPrint) != -1)
       return super.getEntityRef(charToPrint);
     return null;
+  }
+
+  /**
+   * Called to serialize a DOM element. Equivalent to calling {@link
+   * #startElement}, {@link #endElement} and serializing everything
+   * inbetween, but better optimized.
+   */
+  @Override
+  protected void serializeElement(Element elem )
+          throws IOException
+  {
+    Attr attr;
+    NamedNodeMap attrMap;
+    int          i;
+    Node child;
+    ElementState state;
+    boolean      preserveSpace;
+    String       name;
+    String       value;
+    String       tagName;
+
+    tagName = elem.getTagName();
+    state = getElementState();
+    if ( isDocumentState() ) {
+      // If this is the root element handle it differently.
+      // If the first root element in the document, serialize
+      // the document's DOCTYPE. Space preserving defaults
+      // to that of the output format.
+      if ( ! _started )
+        startDocument( tagName );
+    } else {
+      // For any other element, if first in parent, then
+      // close parent's opening tag and use the parnet's
+      // space preserving.
+      if ( state.empty )
+        _printer.printText( '>' );
+      // Indent this element on a new line if the first
+      // content of the parent element or immediately
+      // following an element.
+      if ( _indenting && ! state.preserveSpace &&
+              ( state.empty || state.afterElement ) )
+        _printer.breakLine();
+    }
+    preserveSpace = state.preserveSpace;
+
+    // Do not change the current element state yet.
+    // This only happens in endElement().
+
+    // XHTML: element names are lower case, DOM will be different
+    _printer.printText( '<' );
+    _printer.printText( tagName );
+    _printer.indent();
+
+    // Lookup the element's attribute, but only print specified
+    // attributes. (Unspecified attributes are derived from the DTD.
+    // For each attribute print it's name and value as one part,
+    // separated with a space so the element can be broken on
+    // multiple lines.
+    attrMap = elem.getAttributes();
+    if ( attrMap != null ) {
+      for ( i = 0 ; i < attrMap.getLength() ; ++i ) {
+        attr = (Attr) attrMap.item( i );
+        name = attr.getName().toLowerCase(Locale.ENGLISH);
+        value = attr.getValue();
+        if ( attr.getSpecified() ) {
+          _printer.printSpace();
+          // HTML: Empty values print as attribute name, no value.
+          // HTML: URI attributes will print unescaped
+          if ( value == null ) {
+            value = "";
+          }
+          if ( !_format.getPreserveEmptyAttributes() && value.length() == 0 )
+            _printer.printText( name );
+          else if ( HTMLdtd.isURI( tagName, name ) ) {
+            _printer.printText( name );
+            _printer.printText( "=\"" );
+            _printer.printText( escapeURI( value ) );
+            _printer.printText( '"' );
+          } else if ( HTMLdtd.isBoolean( tagName, name ) )
+            _printer.printText( name );
+          else {
+            _printer.printText( name );
+            _printer.printText( "=\"" );
+            printEscaped( value );
+            _printer.printText( '"' );
+          }
+        }
+      }
+    }
+    if ( HTMLdtd.isPreserveSpace( tagName ) )
+      preserveSpace = true;
+
+    // If element has children, or if element is not an empty tag,
+    // serialize an opening tag.
+    if ( elem.hasChildNodes() || ! HTMLdtd.isEmptyTag( tagName ) ) {
+      // Enter an element state, and serialize the children
+      // one by one. Finally, end the element.
+      state = enterElementState( null, null, tagName, preserveSpace );
+
+      // Prevents line breaks inside A/TD
+      if ( tagName.equalsIgnoreCase( "A" ) || tagName.equalsIgnoreCase( "TD" ) ) {
+        state.empty = false;
+        _printer.printText( '>' );
+      }
+
+      // Handle SCRIPT and STYLE specifically by changing the
+      // state of the current element to CDATA (XHTML) or
+      // unescaped (HTML).
+      if ( tagName.equalsIgnoreCase( "SCRIPT" ) ||
+              tagName.equalsIgnoreCase( "STYLE" ) ) {
+        // HTML: Print contents unescaped
+        state.unescaped = true;
+      }
+      child = elem.getFirstChild();
+      while ( child != null ) {
+        serializeNode( child );
+        child = child.getNextSibling();
+      }
+      endElementIO( null, null, tagName );
+    } else {
+      _printer.unindent();
+      // XHTML: Close empty tag with ' />' so it's XML and HTML compatible.
+      // HTML: Empty tags are defined as such in DTD no in document.
+      if (!elem.hasChildNodes() && isAllowedEmptyTag(tagName) && !requiresClosingTag(tagName))
+        _printer.printText( "/>" );
+      else
+        _printer.printText( '>' );
+      // After element but parent element is no longer empty.
+      state.afterElement = true;
+      state.empty = false;
+      if ( isDocumentState() )
+        _printer.flush();
+    }
   }
 
   @Override
